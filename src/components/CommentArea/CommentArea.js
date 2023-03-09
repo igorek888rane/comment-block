86a6a6bf81c48d7fa5f3ef00b0031e0ab5b4fd14
igor <igorek888rane@gmail.com>
2023-03-09 09:18:48 +0300

    like

--- src/components/CommentArea/CommentArea.js
+++ src/components/CommentArea/CommentArea.js
@@ -1,6 +1,9 @@
 import createElement from '../../utils/createElement'
 import CommentEl from "./Comment";
 import getComments from "../../utils/getComments";
+import like from '../../img/comment/like_icon.svg'
+import likeAdd from '../../img/comment/like_icon_add.svg'
+
 
 class CommentArea {
     constructor(comments) {
@@ -26,16 +29,23 @@ class CommentArea {
     delete(e) {
         if (e.target.closest('.comment__delete')) {
             const id = e.target.dataset.id
-            console.log(id);
-            let comments = getComments().filter(com => com.id !== +id)
-            console.log(comments);
-
+            const comments = getComments().filter(com => com.id !== +id)
             document.getElementById(id).remove()
             localStorage.setItem('comments', JSON.stringify(comments))
         }
     }
 
     like(e) {
+        if (e.target.closest('.comment__like')) {
+            const id = e.target.dataset.id
+            const comment = document.getElementById(id)
+            comment.classList.toggle('like')
+            comment
+                .querySelector('.comment__like')
+                .src = comment.classList.value === 'comment__item like'
+                ? likeAdd
+                : like
+        }
 
     }
 
